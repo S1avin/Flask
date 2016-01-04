192e3d453e1323fbe4dea12976e0b6595421ebdf
S1avin <nik.ibragimov@gmail.com>
2016-01-04 10:12:42 -0600

    My own

--- __init__.py
+++ __init__.py
@@ -1,47 +1,60 @@
 from flask import Flask, render_template
- 
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
 
-    title = "Epic Tutorials"
-    paragraph = ["wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!","wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!"]
+    title = "Empty page"
+    paragraph = ["Nothing here right now"]
 
     try:
         return render_template("index.html", title = title, paragraph=paragraph)
     except Exception, e:
         return str(e)
 
-@app.route('/about')
-def aboutpage():
+@app.route('/new')
+def nawpage():
 
-    title = "About this site"
-    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
-
-    pageType = 'about'
-
-    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
-
-
-@app.route('/about/contact')
-def contactPage():
-
-    title = "About this site"
-    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
-
-    pageType = 'about'
-
-    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
-
-@app.route('/z')
-def Page():
-
-    title = "ZZZZZZzzzzzzzzzz......"
-    paragraph = ["Wow"]
+    title = "Add new Item"
+    paragraph = ["Here will be some input boxes."]
 
     return render_template("index.html", title=title, paragraph=paragraph)
 
- 
+@app.route('/update')
+def updatePage():
+
+    title = "update"
+    paragraph = ["update"]
+
+    return render_template("index.html", title=title, paragraph=paragraph)
+
+@app.route('/update/price')
+def updatepricePage():
+
+    title = "update price"
+    paragraph = ["You can update any price here"]
+
+    return render_template("index.html", title=title, paragraph=paragraph)
+
+@app.route('/update/description')
+def updatedescriptionPage():
+
+    title = "update description"
+    paragraph = ["You can update any description here"]
+
+    return render_template("index.html", title=title, paragraph=paragraph)
+
+@app.route('/update/location')
+def updatelocationPage():
+
+    title = "update location"
+    paragraph = ["You can update any location here"]
+
+    return render_template("index.html", title=title, paragraph=paragraph)
+
+
 if __name__ == "__main__":
-	app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
+        app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
+
+
